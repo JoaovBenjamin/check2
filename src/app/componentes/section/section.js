3c802fe1cd440abe4c685966d14a7039c60d0f05
Joaovvb <rm98938@fiap.com.br>
2023-09-29 21:44:09 -0300

    Jevs

--- src/app/componentes/section/section.js
+++ src/app/componentes/section/section.js
@@ -2,3 +2,9 @@
 
 import { styled } from "styled-components";
 
+export const Section = styled.section`
+    display: flex;
+    background-color: white;
+    align-items: center;
+    flex-direction: column;
+`
